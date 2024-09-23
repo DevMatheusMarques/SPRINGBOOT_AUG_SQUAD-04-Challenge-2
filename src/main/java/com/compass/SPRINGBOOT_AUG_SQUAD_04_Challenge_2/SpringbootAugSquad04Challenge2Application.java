2a0b784625a252beb0a8a809cbcdfef64bc0211d
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-09-23 17:16:14 -0300

    feat: Project created and established database connection configuration

---
+++ src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/SpringbootAugSquad04Challenge2Application.java
@@ -0,0 +1,13 @@
+package com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAugSquad04Challenge2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAugSquad04Challenge2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/SpringbootAugSquad04Challenge2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAugSquad04Challenge2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
