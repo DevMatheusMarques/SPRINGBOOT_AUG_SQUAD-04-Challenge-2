8000fa15b5d2be9227ef4db583567c1e6d9a9b24
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-09-24 17:05:17 -0300

    feat: Created directories

---
+++ src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/repositories/Teste.java
@@ -0,0 +1,4 @@
+package com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2.repositories;
+
+public class Teste {
+}
---
+++ src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/services/Teste.java
@@ -0,0 +1,4 @@
+package com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2.services;
+
+public class Teste {
+}
