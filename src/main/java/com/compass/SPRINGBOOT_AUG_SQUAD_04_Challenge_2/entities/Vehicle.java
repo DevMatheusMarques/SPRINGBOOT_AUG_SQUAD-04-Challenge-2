1ea5ba13810999403377fac1b93d1787179369be
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-09-25 12:31:52 -0300

    feat: Vehicle entity completed

---
+++ src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/entities/Vehicle.java
@@ -0,0 +1,43 @@
+package com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = "vehicles")
+@Entity
+public class Vehicle implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "plate", nullable = false, length = 9)
+    private String plate;
+    @Column(name = "type", nullable = false, length = 20)
+    private String type;
+    @Column(name = "monthly", nullable = false)
+    private Boolean monthly;
+
+    @CreatedDate
+    @Column(name = "date_created")
+    private LocalDateTime dateCreated;
+    @LastModifiedDate
+    @Column(name = "date_modified")
+    private LocalDateTime dateModified;
+
+    @OneToOne
+    @JoinColumn(name = "vacany.id", referencedColumnName = "id")
+    private Vacancy vacancy;
+
+    @OneToOne(mappedBy = "vehicle")
+    private Ticket ticket;
+}
