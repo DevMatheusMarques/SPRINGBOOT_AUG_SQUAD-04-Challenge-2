8de8af1544d7b3345de0e38b4602a881ac57a908
JorgeMassaru <jorge.hashiguchi2005@gmail.com>
2024-09-26 15:43:08 -0300

    Sending changes in 'VacancyController'

--- src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/web/controller/VacancyController.java
+++ src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/web/controller/VacancyController.java
@@ -5,6 +5,7 @@ import com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2.services.VacancyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -15,10 +16,17 @@ public class VacancyController {
     private VacancyService vacancyService;
 
     @GetMapping("/{id}")
-    public Vacancy getVacancyById(@PathVariable Long id) {
-        return vacancyService.getVacancyById(id);
-    }
+    public Map<String, Object> getVacancyById(@PathVariable Long id) {
+        Vacancy vacancy = vacancyService.getVacancyById(id);
 
+        Map<String, Object> response = new HashMap<>();
+        response.put("avulsas_ocupadas", vacancy.getSeparated_ocuppied());
+        response.put("capacidade_avulsas", vacancy.getSeparated_capacity());
+        response.put("mensalistas_ocupadas", vacancy.getMonthly_occupied());
+        response.put("capacidade_mensalistas", vacancy.getMonthly_capacity());
+
+        return response;
+    }
     @PatchMapping("/{id}")
     public Vacancy updateVacancyCapacity(@PathVariable Long id,
                                          @RequestBody Map<String, Double> capacities) {
