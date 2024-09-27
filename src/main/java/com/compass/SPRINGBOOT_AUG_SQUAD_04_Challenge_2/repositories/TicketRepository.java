8135aa030e78f15d0e7a887f24cb0ccbc7d5f1a6
Lívia <liviaportelaferreira@gmail.com>
2024-09-26 22:30:12 -0300

    refactor: Adjusted TicketRepository

--- src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/repositories/TicketRepository.java
+++ src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/repositories/TicketRepository.java
@@ -2,10 +2,15 @@ package com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2.repositories;
 
 import com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2.entities.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
-    Optional<Ticket> findByVehicle_Plate(String plate);
+    List<Ticket> findByVehicle_Plate(String plate);
+
+    @Query("SELECT t FROM Ticket t WHERE t.vehicle.plate = :plate AND t.parked = TRUE AND t.dateTimeExit IS NULL")
+    Ticket findActiveTicketByVehiclePlate(@Param("plate") String plate);
 }
