ed93f586a5c1f4795683132533d2b04648667b25
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-09-27 14:39:20 -0300

    refactor: Adjusted the cancel class to return exceptions

---
+++ src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/exceptions/UnauthorizedCancelAccessException.java
@@ -0,0 +1,7 @@
+package com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2.exceptions;
+
+public class UnauthorizedCancelAccessException extends RuntimeException {
+    public UnauthorizedCancelAccessException(String message) {
+        super(message);
+    }
+}
