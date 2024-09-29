6f20e9f495a91fc87dc546b2b8fdd20f30d77d3f
Rafael Garcia <rafaelbackup666@gmail.com>
2024-09-29 16:44:57 -0300

    DTO and Mapper Inclusion

---
+++ src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/web/dto/VehicleCreateDto.java
@@ -0,0 +1,14 @@
+package com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class VehicleCreateDto {
+    private String plate;
+    private String type;
+    private String category;
+}
