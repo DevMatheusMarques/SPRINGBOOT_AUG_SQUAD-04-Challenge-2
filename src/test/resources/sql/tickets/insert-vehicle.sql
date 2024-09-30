6ba34048a6e30fd2debc54910ca2d130c55d3c3c
Gustavo Bonfim <dev.gusta.io@gmail.com>
2024-09-30 15:21:04 -0300

    test: structure tests and initial tickets integration tests

---
+++ src/test/resources/sql/tickets/insert-vehicle.sql
@@ -0,0 +1,17 @@
+INSERT INTO VEHICLES (id, category, plate, registered ,type_vehicle)
+VALUES (1, 1, 'ABC-0000', 1, 1);
+
+INSERT INTO VEHICLES (id, category, plate, registered ,type_vehicle)
+VALUES (2, 'MONTHLY_PAYER', 'ABC-1111', 1, 'PASSENGER_CAR');
+
+INSERT INTO VEHICLES (id, category, plate, registered ,type_vehicle)
+VALUES (3, 'SINGLE', 'ABC-2222', 1, 'MOTORCYCLE');
+
+INSERT INTO VEHICLES (id, category, plate, registered ,type_vehicle)
+VALUES (4, 'MONTHLY_PAYER', 'ABC-3333', 1, 'MOTORCYCLE');
+
+INSERT INTO VEHICLES (id, category, plate, registered ,type_vehicle)
+VALUES (5, 'DELIVERY_TRUCK', 'ABC-4444', 1, 'DELIVERY_TRUCK');
+
+INSERT INTO VEHICLES (id, category, plate, registered ,type_vehicle)
+VALUES (6, 'PUBLIC_SERVICE', 'ABC-5555', 1, 'PUBLIC_SERVICE');
