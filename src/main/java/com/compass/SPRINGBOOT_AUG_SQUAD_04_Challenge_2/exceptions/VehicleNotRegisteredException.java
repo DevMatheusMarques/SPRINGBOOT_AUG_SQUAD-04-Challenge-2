0e8e60667ecf0213cbb7a172a7b6760c2c9decb0
Lívia <liviaportelaferreira@gmail.com>
2024-09-29 23:51:12 -0300

    feature: Exception VehicleNotRegisteredException

---
+++ src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/exceptions/VehicleNotRegisteredException.java
@@ -0,0 +1,7 @@
+package com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2.exceptions;
+
+public class VehicleNotRegisteredException extends RuntimeException {
+  public VehicleNotRegisteredException(String message) {
+    super(message);
+  }
+}
