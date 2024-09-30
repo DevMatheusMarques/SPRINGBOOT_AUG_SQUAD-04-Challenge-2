eeeff64b57e3c992f617e92f4edfeeb564c2cb7a
LucasGeneroso15 <lucas.siilva12@live.com>
2024-09-30 14:52:30 -0300

    fix: adjusting parking class

--- src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/exceptions/NoVacanciesAvailableException.java
+++ src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/exceptions/NoVacanciesAvailableException.java
@@ -1,6 +1,6 @@
 package com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2.exceptions;
 
-public class NoVacanciesAvailableException extends Throwable {
+public class NoVacanciesAvailableException extends RuntimeException {
     public NoVacanciesAvailableException(String message) {
         super(message);
     }
