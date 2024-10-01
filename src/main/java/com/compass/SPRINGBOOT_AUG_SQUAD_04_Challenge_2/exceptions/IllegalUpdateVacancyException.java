5ecefd35f48d9d2a31dc0f2698224259b6ca1034
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-10-01 02:59:56 -0300

    docs: Finalized documentation of exception classes

--- src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/exceptions/IllegalUpdateVacancyException.java
+++ src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/exceptions/IllegalUpdateVacancyException.java
@@ -1,6 +1,17 @@
 package com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2.exceptions;
 
+/**
+ * This class represents a custom exception named IllegalUpdateVacancyException,
+ * which is thrown when an illegal or invalid update operation is attempted on a vacancy.
+ * It extends RuntimeException, meaning it is an unchecked exception.
+ */
 public class IllegalUpdateVacancyException extends RuntimeException {
+
+    /**
+     * Constructor for IllegalUpdateVacancyException that accepts a custom error message.
+     *
+     * @param message The detailed error message explaining the cause of the exception.
+     */
     public IllegalUpdateVacancyException(String message) {
         super(message);
     }
--- src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/exceptions/InvalidExitCancelException.java
+++ src/main/java/com/compass/SPRINGBOOT_AUG_SQUAD_04_Challenge_2/exceptions/InvalidExitCancelException.java
@@ -1,6 +1,17 @@
 package com.compass.SPRINGBOOT_AUG_SQUAD_04_Challenge_2.exceptions;
 
+/**
+ * This class represents a custom exception named InvalidExitCancelException,
+ * which is thrown when there is an invalid exit operation related to the parking cancel.
+ * It extends RuntimeException, making it an unchecked exception.
+ */
 public class InvalidExitCancelException extends RuntimeException {
+
+    /**
+     * Constructor for InvalidExitCancelException that accepts a custom error message.
+     *
+     * @param message The detailed error message explaining the cause of the exception.
+     */
     public InvalidExitCancelException(String message) {
         super(message);
     }
